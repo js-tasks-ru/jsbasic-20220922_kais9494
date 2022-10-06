60e8ca4dd1e5a2b4ff389fd35e53e7bf951b1ae5
dima_vch <109754944+DimaVoiceh@users.noreply.github.com>
2022-10-06 12:19:14 +0300

    2-module tastls

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let salary in salaries) {
+    if (Number.isFinite(salaries[salary])) {
+      sum += salaries[salary];
+    }
+  }
+  return sum;
 }
