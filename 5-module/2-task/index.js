4f3babdf475ed740e2a6a0cdf8fc42762b4e0cc0
dima_vch <109754944+DimaVoiceh@users.noreply.github.com>
2022-10-19 23:20:08 +0300

    5-module
    5-module solutions

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  btn.addEventListener('click', (e) => {
+    let text = document.querySelector('#text');
+    text.hidden = !text.hidden;
+  })
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
-  // ваш код...
+  let activeSlide = {
+    num: 1,
+    offset: 0,
+  }
+  let slides = document.querySelectorAll('.carousel__slide');
+
+  if (slides && slides.length) {
+    let slidesQty = slides.length;
+    let prevBtn = document.querySelector('.carousel__arrow_left');
+    let nextBtn = document.querySelector('.carousel__arrow_right');
+    let sliderWrapper = document.querySelector('.carousel__inner');
+
+    nextBtn.style.display = activeSlide.num == slidesQty ? 'none' : '';
+    prevBtn.style.display = activeSlide.num == 1 ? 'none' : '';
+
+    document.addEventListener('click', (e) => {
+      if (e.target.closest('.carousel__arrow')) {
+        let targetBtn = e.target.closest('.carousel__arrow');
+        let slideCurrent = slides[activeSlide.num - 1];
+        let slideWidth = slideCurrent.offsetWidth;
+        if (targetBtn == nextBtn && activeSlide.num < slidesQty) {
+          activeSlide.offset -= slideWidth;
+          activeSlide.num++;
+          sliderWrapper.style.transform = `translateX(${activeSlide.offset}px)`
+        }
+        else if (targetBtn == prevBtn && activeSlide.num > 1) {
+          activeSlide.offset += slideWidth;
+          activeSlide.num--;
+          sliderWrapper.style.transform = `translateX(${activeSlide.offset}px)`
+        }
+
+        nextBtn.style.display = activeSlide.num == slidesQty ? 'none' : '';
+        prevBtn.style.display = activeSlide.num == 1 ? 'none' : '';
+
+      }
+    })
+  }
 }
+
